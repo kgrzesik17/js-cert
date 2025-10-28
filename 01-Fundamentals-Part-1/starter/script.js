4a74ae97f6ec0ba31e0214c57ca8e4fa268dae8d
Kacper <kgrzesik17@gmail.com>
2025-10-28 15:17:39 +0100

    values and variables

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,16 @@
-let js = 'amazing';
-if(js === 'amazing') alert('JavaScript is FUN');
+// let js = 'amazing';
 
-console.log(40 + 8 + 23 - 10);
+// console.log(40 + 8 + 23 - 10);
+
+// console.log('Jonas')
+// console.log(23);
+
+// let firstName = "Jonas";
+// console.log(firstName);
+
+// ASSIGNMENT 1.1
+let country = 'Poland';
+let continent = 'Europe';
+let population = 40000000;
+
+console.log(country, continent, population);
