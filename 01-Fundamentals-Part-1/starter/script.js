e0231ff346f2e7ec8a037801df4d74289d8e78fa
Kacper <kgrzesik17@gmail.com>
2025-10-28 18:01:16 +0100

    strings and template literals

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -25,29 +25,38 @@
 
 // const job;
 
-const now = 2037;
-const ageJonas = now - 1991;
-const ageSarah = now - 2018;
-console.log(ageJonas, ageSarah);
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+// console.log(ageJonas, ageSarah);
 
-// math operators
-console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
-const firstName = "Kacper";
-const lastName = "Grzesik";
-console.log(firstName + ' ' + lastName);
+// // math operators
+// console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// const firstName = "Kacper";
+// const lastName = "Grzesik";
+// console.log(firstName + ' ' + lastName);
 
-// asignment operators
-let x = 10 + 5;
-x += 10;
-x *= 4;
-x++;
-x--;
-console.log(x);
+// // asignment operators
+// let x = 10 + 5;
+// x += 10;
+// x *= 4;
+// x++;
+// x--;
+// console.log(x);
 
-// comparison operators
-console.log(ageJonas > ageSarah);
-console.log(ageSarah >= 18);
+// // comparison operators
+// console.log(ageJonas > ageSarah);
+// console.log(ageSarah >= 18);
 
-const isFullAge = ageSarah >= 18;
+// const isFullAge = ageSarah >= 18;
 
-console.log(now - 1991 > now - 2018);
+// console.log(now - 1991 > now - 2018);
+
+const firstName = 'Jonas'
+const job = 'teacher'
+const birthYear = 1991;
+const year = 2037;
+
+const jonas = `I'm ${firstName}. I'm a ${year - birthYear} year old ${job}.`
+
+console.log(jonas);
