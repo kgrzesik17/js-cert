a830d92ad6aa3bf71dc97d80285d865559da3fcb
Kacper <kgrzesik17@gmail.com>
2025-10-28 15:35:48 +0100

    operators

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,4 @@
+
 // let js = 'amazing';
 
 // console.log(40 + 8 + 23 - 10);
@@ -8,9 +9,45 @@
 // let firstName = "Jonas";
 // console.log(firstName);
 
-// ASSIGNMENT 1.1
-let country = 'Poland';
-let continent = 'Europe';
-let population = 40000000;
+// // ASSIGNMENT 1.1
+// let country = 'Poland';
+// let continent = 'Europe';
+// let population = 40000000;
 
-console.log(country, continent, population);
+// console.log(country, continent, population);
+
+
+// let age = 30;
+// age = 31;
+
+// const birthYear = 1991;
+// // birthYear = 1990;
+
+// const job;
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+// math operators
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+const firstName = "Kacper";
+const lastName = "Grzesik";
+console.log(firstName + ' ' + lastName);
+
+// asignment operators
+let x = 10 + 5;
+x += 10;
+x *= 4;
+x++;
+x--;
+console.log(x);
+
+// comparison operators
+console.log(ageJonas > ageSarah);
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
