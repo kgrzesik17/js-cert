01e34946018d8774c9daef105f864ae56adb6ed8
Kacper <kgrzesik17@gmail.com>
2025-10-29 21:09:43 +0100

    challenge 7

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -209,35 +209,109 @@
 
 // console.log(typeof jonas.firstName);
 
-const jonas = {
-    firstName: 'Jonas',
-    lastName: 'Schmedtmann',
-    age: 2037 - 1991,
-    job: 'teacher',
-    friends: ['Michael', 'Peter', 'Steven']
-};
+// const jonas = {
+//     firstName: 'Jonas',
+//     lastName: 'Schmedtmann',
+//     age: 2037 - 1991,
+//     job: 'teacher',
+//     friends: ['Michael', 'Peter', 'Steven']
+// };
 
-console.log(jonas);
+// console.log(jonas);
 
-console.log(jonas.lastName);
-console.log(jonas['lastName']);
+// console.log(jonas.lastName);
+// console.log(jonas['lastName']);
 
-const nameKey = 'Name';
-console.log(jonas['first' + nameKey]);
-console.log(jonas['last' + nameKey]);
+// const nameKey = 'Name';
+// console.log(jonas['first' + nameKey]);
+// console.log(jonas['last' + nameKey]);
 
-const interestedIn = prompt('What do you want to know about jonas? Choose between firstName, lastName, age, job, and friends');
+// const interestedIn = prompt('What do you want to know about jonas? Choose between firstName, lastName, age, job, and friends');
 
-if(jonas[interestedIn]) {
-    console.log(jonas[interestedIn]);
-} else {
-    console.log('Wrong request! Choose between firstName, lastName, age, job, and friends');
+// if(jonas[interestedIn]) {
+//     console.log(jonas[interestedIn]);
+// } else {
+//     console.log('Wrong request! Choose between firstName, lastName, age, job, and friends');
+// }
+
+// jonas.location = 'Portugal';
+// jonas['twitter'] = '@jonasschmedtman';
+
+// console.log(jonas);
+
+// // challenge
+// console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`);
+
+// const jonas = {
+//     firstName: 'Jonas',
+//     lastName: 'Schmedtmann',
+//     birthYear: 1991,
+//     job: 'teacher',
+//     friends: ['Michael', 'Peter', 'Steven'],
+//     hasDriversLicense: true,
+
+//     // calcAge: birthYear => 2037 - birthYear
+
+//     // calcAge: function() {
+//     //     // console.log(this);
+//     //     return 2037 - this.birthYear;
+//     // }
+
+//     calcAge: function() {
+//         this.age = 2037 - this.birthYear;
+//         return this.age;
+//     },
+
+//     // this does NOT work because arrow functions dont have a this keyword!!!
+//     // getInfo: () => `${this.firstName} is a ${this.age}-year old teacher, and he has ${this.hasDriversLicense ? 'a' : 'no'} driver's license`
+
+//     getSummary: function() {
+//         return `${this.firstName} is a ${this.age}-year old teacher, and he has ${this.hasDriversLicense ? 'a' : 'no'} driver's license`
+//     }
+// };
+
+// console.log(jonas.calcAge());
+
+// console.log(jonas.age);
+// console.log(jonas.age);
+// console.log(jonas.age);
+// console.log(jonas.age);
+// // console.log(jonas[' calcAge'](1991));
+
+// // challenge
+// // "Jonas is a 46-year old teacher, and he has a/no driver's license."
+
+// console.log(jonas.getSummary());
+
+const mark = {
+    fullName: 'Mark Miller',
+    mass: '78',
+    height: '1.69',
+
+    calcBMI: function () {
+        this.bmi = this.mass / (this.height * this.height);
+        return this.bmi;
+    }
 }
 
-jonas.location = 'Portugal';
-jonas['twitter'] = '@jonasschmedtman';
+const john = {
+    fullName: 'John Smith',
+    mass: '92',
+    height: '1.95',
 
-console.log(jonas);
+    calcBMI: function () {
+        this.bmi = this.mass / (this.height * this.height);
+        return this.bmi;
+    }
+}
 
-// challenge
-console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`);
+console.log(mark.calcBMI());
+console.log(john.calcBMI());
+
+if (mark.bmi > john.bmi) {
+    console.log(`${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s BMI (${john.bmi})`);
+} else if (john.bmi > mark.bmi) {
+    console.log(`${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s BMI (${mark.bmi})`);
+} else {
+    console.log(`${mark.fullName}'s and ${john.fullName}'s BMIs are the same`);
+}
