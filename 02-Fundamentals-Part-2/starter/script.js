a8214ab5c5244ab9fae0b250a1d0756e695502e4
Kacper <kgrzesik17@gmail.com>
2025-10-29 12:22:18 +0100

    anonymous function

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,47 @@
+// 'use strict';
+
+// let hasDriversLicense = false;
+// const passTest = true;
+
+// if (passTest) hasDriversLicense = true;
+// if (hasDriversLicense) console.log('I can drive :D');
+
+// // strict mode showcase
+// const interface = 'Audio';
+// const private = 534;
+// const if = 23;
+
+// function logger() {
+//     console.log('My name is Jonas');
+// }
+
+// // calling / running / invoking the function
+// logger();
+// logger();
+// logger();
+
+// function fruitProcessor(apples, oranges) {
+//     console.log(apples, oranges);
+//     const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+//     return juice;
+// }
+
+// const appleJuice = fruitProcessor(5, 0);
+// console.log(appleJuice);
+// // console.log(fruitProcessor(5, 0));
+
+// const appleOrangeJuice = fruitProcessor(2, 4);
+// console.log(appleOrangeJuice);
+
+function calcAge1(birthYear) {
+    return 2037 - birthYear;
+}
+
+const age1 = calcAge1(1991);
+console.log(age1);
+
+const calcAge2 = function (birthYear) {
+    return 2037 - birthYear;
+}
+const age2 = calcAge2(1991);
+console.log(age2);
