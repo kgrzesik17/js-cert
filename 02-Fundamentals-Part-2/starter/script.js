702f0ae0a24f16b1a1dcc6200a30ee4de59d7f6c
Kacper <kgrzesik17@gmail.com>
2025-10-29 12:29:29 +0100

    arrow functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -33,15 +33,28 @@
 // const appleOrangeJuice = fruitProcessor(2, 4);
 // console.log(appleOrangeJuice);
 
-function calcAge1(birthYear) {
-    return 2037 - birthYear;
-}
+// function calcAge1(birthYear) {
+//     return 2037 - birthYear;
+// }
 
-const age1 = calcAge1(1991);
-console.log(age1);
+// const age1 = calcAge1(1991);
+// console.log(age1);
 
-const calcAge2 = function (birthYear) {
-    return 2037 - birthYear;
+// const calcAge2 = function (birthYear) {
+//     return 2037 - birthYear;
+// }
+// const age2 = calcAge2(1991);
+// console.log(age2);
+
+// arrow function
+const calcAge3 = birthYear => 2037 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2037- birthYear;
+    const retirement = 65 - age;
+    return `${firstName} retires in ${retirement} years.`
 }
-const age2 = calcAge2(1991);
-console.log(age2);
+console.log(yearsUntilRetirement(1991, 'Kacper'));
+console.log(yearsUntilRetirement(1980, 'Bob'));
