52a3084997a2fdc639be6a6b773239343abb6d03
Kacper <kgrzesik17@gmail.com>
2025-10-29 21:47:30 +0100

    challenge 8

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -392,18 +392,49 @@
 //     console.log(`FOR: Lifting weights repetition ${rep}`);
 // }
 
-let rep = 1;
-while (rep <= 10) {
-    console.log(`WHILE: Lifting weights repetition ${rep}`);
-    rep++;
+// let rep = 1;
+// while (rep <= 10) {
+//     console.log(`WHILE: Lifting weights repetition ${rep}`);
+//     rep++;
+// }
+
+// let dice = Math.trunc(Math.random() * 6) + 1;
+// console.log(dice);
+
+// while (dice !== 6) {
+//     console.log(`You rolled a ${dice}`);
+//     dice = Math.trunc(Math.random() * 6) + 1;
+
+//     if (dice === 6) console.log('Loop is about to end...');
+// }
+
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 }
 
-let dice = Math.trunc(Math.random() * 6) + 1;
-console.log(dice);
+/* Write your code below. Good luck! 🙂 */
 
-while (dice !== 6) {
-    console.log(`You rolled a ${dice}`);
-    dice = Math.trunc(Math.random() * 6) + 1;
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
 
-    if (dice === 6) console.log('Loop is about to end...');
+for (let i = 0; i < bills.length; i++) {
+    tips.push(calcTip(bills[i]));
+    totals.push(bills[i] + tips[i]);
 }
+
+console.log('Bills: ', bills);
+console.log('Tips: ', tips);
+console.log('Totals: ', totals);
+
+function calcAverage(arr) {
+    let sum = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+
+    return sum / arr.length;
+}
+
+console.log(calcAverage(totals));
