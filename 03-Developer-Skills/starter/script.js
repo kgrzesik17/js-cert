036c763814123618f75426ab50f112c2f09678cb
Kacper <kgrzesik17@gmail.com>
2025-10-30 11:01:36 +0100

    debugging with console and breakpoints

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -12,53 +12,98 @@
 
 // console.log(calcAge(1991));
 
-const temperatures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
+// const temperatures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
 
-// 1) understanding the problem
-// - what is temp amplitude - difference between highest and lowerst temp
-// - how do we caltulate highest and lowest temperatures
-// - what does a sensor error look like? what to do if one occurs?
+// // 1) understanding the problem
+// // - what is temp amplitude - difference between highest and lowerst temp
+// // - how do we caltulate highest and lowest temperatures
+// // - what does a sensor error look like? what to do if one occurs?
 
-// 2) breaking into sub-problems
-// - how to ignore errors
-// - find max value in temp array
-// - find min value in temp array
-// - subtract min from max and return it
+// // 2) breaking into sub-problems
+// // - how to ignore errors
+// // - find max value in temp array
+// // - find min value in temp array
+// // - subtract min from max and return it
 
-function calcTempAmplitude(temps) {
-  let max = temps[0];
-  let min = temps[0];
+// function calcTempAmplitude(temps) {
+//   let max = temps[0];
+//   let min = temps[0];
 
-  for (let i = 0; i < temps.length; i++) {
-    const curTemp = temps[i];
-    if (typeof curTemp !== "number") continue;
+//   for (let i = 0; i < temps.length; i++) {
+//     const curTemp = temps[i];
+//     if (typeof curTemp !== "number") continue;
 
-    if (curTemp > max) max = curTemp;
-    if (curTemp < min) min = curTemp;
-  }
+//     if (curTemp > max) max = curTemp;
+//     if (curTemp < min) min = curTemp;
+//   }
 
-  console.log(max, min);
-  return max - min;
+//   console.log(max, min);
+//   return max - min;
+// }
+
+// const amplitude = calcTempAmplitude(temperatures);
+// console.log(amplitude);
+
+// // problem 2:
+// // function should now recieve 2 arrays of temps
+
+// // 1) understanding the problem
+// // - with 2 arrays, do we need to implement functionality twice? - no! just merge two arrays
+
+// // 2) breaking into sub-problems
+// // merge 2 arrays
+
+// function calcTempAmplitudeNew(t1, t2) {
+//   const temps = t1.concat(t2);
+//   console.log(temps);
+
+//   let max = temps[0];
+//   let min = temps[0];
+
+//   for (let i = 0; i < temps.length; i++) {
+//     const curTemp = temps[i];
+//     if (typeof curTemp !== "number") continue;
+
+//     if (curTemp > max) max = curTemp;
+// 10   if (curTemp < min) min = curTemp;
+//   }
+
+//   console.log(max, min);
+//   return max - min;
+// }
+
+// const amplitudeNew = calcTempAmplitudeNew([3, 5, 1], [9, 0, 5]);
+// console.log(amplitudeNew);
+
+function measureKelvin() {
+  const measurement = {
+    type: "temp",
+    unit: "celsius",
+
+    // c) fix the bug
+    // value: Number(prompt("Degrees celcius:")),
+    value: 10,
+  };
+
+  // b) find the bug
+  // console.log(measurement);
+  console.table(measurement);
+  // console.log(measurement.value);
+
+  const kelvin = measurement.value + 273;
+  return kelvin;
 }
 
-const amplitude = calcTempAmplitude(temperatures);
-console.log(amplitude);
+// a) identify the bug
+console.log(measureKelvin());
 
-// problem 2:
-// function should now recieve 2 arrays of temps
-
-// 1) understanding the problem
-// - with 2 arrays, do we need to implement functionality twice? - no! just merge two arrays
-
-// 2) breaking into sub-problems
-// merge 2 arrays
-
-function calcTempAmplitudeNew(t1, t2) {
+// using a debugger
+function calcTempAmplitudeBug(t1, t2) {
   const temps = t1.concat(t2);
   console.log(temps);
 
-  let max = temps[0];
-  let min = temps[0];
+  let max = 0;
+  let min = 0;
 
   for (let i = 0; i < temps.length; i++) {
     const curTemp = temps[i];
@@ -72,5 +117,6 @@ function calcTempAmplitudeNew(t1, t2) {
   return max - min;
 }
 
-const amplitudeNew = calcTempAmplitudeNew([3, 5, 1], [9, 0, 5]);
-console.log(amplitudeNew);
+const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 1, 5]);
+// a) identify
+console.log(amplitudeBug);
