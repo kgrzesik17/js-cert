bc8c2c8e389862323743f78d369d005407f89554
Kacper <kgrzesik17@gmail.com>
2025-10-30 11:14:10 +0100

    challenge 3.1

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -75,48 +75,86 @@
 // const amplitudeNew = calcTempAmplitudeNew([3, 5, 1], [9, 0, 5]);
 // console.log(amplitudeNew);
 
-function measureKelvin() {
-  const measurement = {
-    type: "temp",
-    unit: "celsius",
+// function measureKelvin() {
+//   const measurement = {
+//     type: "temp",
+//     unit: "celsius",
 
-    // c) fix the bug
-    // value: Number(prompt("Degrees celcius:")),
-    value: 10,
-  };
+//     // c) fix the bug
+//     // value: Number(prompt("Degrees celcius:")),
+//     value: 10,
+//   };
 
-  // b) find the bug
-  // console.log(measurement);
-  console.table(measurement);
-  // console.log(measurement.value);
+//   // b) find the bug
+//   // console.log(measurement);
+//   console.table(measurement);
+//   // console.log(measurement.value);
 
-  const kelvin = measurement.value + 273;
-  return kelvin;
-}
+//   const kelvin = measurement.value + 273;
+//   return kelvin;
+// }
 
-// a) identify the bug
-console.log(measureKelvin());
+// // a) identify the bug
+// console.log(measureKelvin());
 
-// using a debugger
-function calcTempAmplitudeBug(t1, t2) {
-  const temps = t1.concat(t2);
-  console.log(temps);
+// // using a debugger
+// function calcTempAmplitudeBug(t1, t2) {
+//   const temps = t1.concat(t2);
+//   console.log(temps);
 
-  let max = 0;
-  let min = 0;
+//   let max = 0;
+//   let min = 0;
 
-  for (let i = 0; i < temps.length; i++) {
-    const curTemp = temps[i];
-    if (typeof curTemp !== "number") continue;
+//   for (let i = 0; i < temps.length; i++) {
+//     const curTemp = temps[i];
+//     if (typeof curTemp !== "number") continue;
 
-    if (curTemp > max) max = curTemp;
-    if (curTemp < min) min = curTemp;
+//     if (curTemp > max) max = curTemp;
+//     if (curTemp < min) min = curTemp;
+//   }
+
+//   console.log(max, min);
+//   return max - min;
+// }
+
+// const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 1, 5]);
+// // a) identify
+// console.log(amplitudeBug);
+
+// CHALLENGE 3.1
+/*
+1. UNDERSTAND THE PROBLEM
+2. DIVIDE AND CONQUER
+- translate arrays to strings 
+- make sure to auto update it
+*/
+
+function printForecast(arr) {
+  let output = "";
+
+  for (let i = 0; i < arr.length; i++) {
+    output += `${arr[i]}°C in ${i + 1} days ... `;
   }
 
-  console.log(max, min);
-  return max - min;
+  return "... " + output;
 }
 
-const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 1, 5]);
-// a) identify
-console.log(amplitudeBug);
+const test1 = [17, 21, 23];
+const test2 = [12, 5, -5, 0, 4];
+
+const forecast1 = printForecast(test1);
+const forecast2 = printForecast(test2);
+console.log(forecast1);
+console.log(forecast2);
+
+// CHALLENGE 3.1 - what it should be
+/*
+1. UNDERSTAND THE PROBLEM
+- array transformed to string, separated by ...
+- what is X days? andwer: index + 1
+2. DIVIDE AND CONQUER
+- transform array into string
+- transform each elements to string with °C
+- strings need to containd day (index + 1)
+- add ... betwee nelements and start and end of the string
+*/
