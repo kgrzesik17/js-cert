2bb4508d2d6798b79bb0a8210b47b28973299781
Kacper <kgrzesik17@gmail.com>
2025-11-03 18:18:11 +0100

    switching the active player

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.querySelector('#current--0');
@@ -16,13 +18,14 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 // rolling dice functionality
 btnRoll.addEventListener('click', function () {
   // 1. generate a random dice rol
   const dice = Math.trunc(Math.random() * 6) + 1;
-  console.log(dice);
 
   // 2. display the dice
   diceEl.classList.remove('hidden');
@@ -31,9 +34,15 @@ btnRoll.addEventListener('click', function () {
   // 3. check for rolled 1
   if (dice !== 1) {
     // add dice to current score
-    current0El.textContent = currentScore; // TODO: change later
     currentScore += dice;
+    document.querySelector(`#current--${activePlayer}`).textContent =
+      currentScore;
   } else {
     // switch to the next player
+    document.querySelector(`#current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
