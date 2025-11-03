6a08bc8de14fbd114f56cc528a8a5906137d4423
Kacper <kgrzesik17@gmail.com>
2025-11-03 18:03:30 +0100

    rolling the dice

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,39 @@
 'use strict';
+
+// selecting elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const current0El = document.querySelector('#current--0');
+const current1El = document.querySelector('#current--1');
+
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+// starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+let currentScore = 0;
+
+// rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  // 1. generate a random dice rol
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+
+  // 2. display the dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  // 3. check for rolled 1
+  if (dice !== 1) {
+    // add dice to current score
+    current0El.textContent = currentScore; // TODO: change later
+    currentScore += dice;
+  } else {
+    // switch to the next player
+  }
+});
