aa86faece2ebeec3bb801acf0f6801c063ff8cd3
Kacper <kgrzesik17@gmail.com>
2025-11-13 12:17:06 +0100

    hoisting and tdz in practice

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 function calcAge(birthYear) {
   const age = 2025 - birthYear;
 
@@ -33,3 +34,45 @@ function calcAge(birthYear) {
 
 const firstName = 'Jonas';
 calcAge(1991);
+*/
+
+// variables
+// console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = 'Jonas';
+let job = 'Teacher';
+const year = 1991;
+
+// functions
+console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+// console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+
+// example
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log('All products deleted!');
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x);
+console.log(y === window.x);
+console.log(z === window.x);
