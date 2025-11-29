48402cfe1c5566cee2fbecbd3402f09e996434c5
Kacper <kgrzesik17@gmail.com>
2025-11-30 00:27:11 +0100

    fighting v1

--- playground-fight/script.js
+++ playground-fight/script.js
@@ -22,9 +22,23 @@ class Player {
   constructor(player_class, strength, constitution) {
     this.damage = strength;
     this.hp = env[player_class].hp_multiplier * constitution;
+    this.currentHp = this.hp;
+  }
+}
+
+function fight(attacker, defender) {
+  console.log(defender.currentHp);
+  while (attacker.currentHp > 0 && defender.currentHp > 0) {
+    let damage_multiplier = Math.random() * (1.25 - 0.75) + 0.75; // damage spread
+    let damage = Math.round(attacker.damage * damage_multiplier);
+
+    defender.currentHp -= damage;
+    console.log(`You hit your opponent for ${damage} damage`);
+    console.log(`He's left with ${defender.currentHp} HP.`);
   }
 }
 
 const p1 = new Player(1, 10, 10);
+const p2 = new Player(2, 10, 10);
 
-console.log(p1.hp);
+fight(p1, p2);
