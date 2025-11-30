5ff4ef424ccc2ecd0ed35f06380079c07b4cd550
Kacper <kgrzesik17@gmail.com>
2025-11-30 09:21:31 +0100

    added critical strikes

--- playground-fight/script.js
+++ playground-fight/script.js
@@ -23,22 +23,33 @@ class Player {
     this.damage = strength;
     this.hp = env[player_class].hp_multiplier * constitution;
     this.currentHp = this.hp;
+    this.critChance = 0.5; // TODO
+    this.critMultiplier = 2;
   }
 }
 
-function fight(attacker, defender) {
-  console.log(defender.currentHp);
-  while (attacker.currentHp > 0 && defender.currentHp > 0) {
-    let damage_multiplier = Math.random() * (1.25 - 0.75) + 0.75; // damage spread
-    let damage = Math.round(attacker.damage * damage_multiplier);
+function calculateSingleHitDamage(attacker, defender) {
+  const damage_multiplier = Math.random() * (1.25 - 0.75) + 0.75; // damage spread
+  const isCrit = Math.random() <= attacker.critChance ? true : false;
 
-    defender.currentHp -= damage;
-    console.log(`You hit your opponent for ${damage} damage`);
-    console.log(`He's left with ${defender.currentHp} HP.`);
+  let damage = attacker.damage * damage_multiplier; // check damge fluctuation
+  if (isCrit) damage *= attacker.critMultiplier; // increase damage if crit
+
+  damage = Math.round(damage);
+
+  console.log([damage, isCrit]);
+
+  // damage, isCrit
+  return [damage, isCrit];
+}
+
+function loop(p1, p2) {
+  for (let i = 0; i < 10; i++) {
+    calculateSingleHitDamage(p1, p2);
   }
 }
 
 const p1 = new Player(1, 10, 10);
 const p2 = new Player(2, 10, 10);
 
-fight(p1, p2);
+calculateSingleHitDamage(p1, p2);
