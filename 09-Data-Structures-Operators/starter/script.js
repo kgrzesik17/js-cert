965744fdd30c12a138e61378c20e3d0d29d4b62b
Kacper <kgrzesik17@gmail.com>
2026-01-04 19:12:05 +0100

    logical assignment operators

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -72,6 +72,38 @@ const restaurant = {
   },
 };
 
+const rest1 = {
+  name: 'Capri',
+  // numGuests: 20,
+  numGuests: 0,
+};
+
+const rest2 = {
+  name: 'Piazza',
+  owner: 'Giovanni Rossi',
+};
+
+// OR assignment operator
+// rest1.numGuests = rest1.numGuests || 10;
+// rest2.numGuests = rest2.numGuests || 10;
+
+// rest1.numGuests ||= 10; // rest1.numGuests = rest1.numGuests || 10;
+// rest2.numGuests ||= 10;
+
+// nullish assignment operator
+rest1.numGuests ??= 10; // rest1.numGuests = rest1.numGuests || 10;
+rest2.numGuests ??= 10;
+
+// AND assignment operator
+// rest1.owner = rest1.owner && '<ANONYMOUS>';
+// rest2.owner = rest2.owner && '<ANONYMOUS>';
+rest1.owner &&= '<ANONYMOUS>'; // rest1.owner = rest1.owner && '<ANONYMOUS>';
+rest2.owner &&= '<ANONYMOUS>';
+
+console.log(rest1);
+console.log(rest2);
+
+/*
 restaurant.numGuests = 0;
 const guests = restaurant.numGuests || 10;
 console.log(guests);
@@ -80,9 +112,9 @@ console.log(guests);
 const guestsCorrect = restaurant.numGuests ?? 10;
 console.log(guestsCorrect);
 
-/*
 // use ANY data type, return ANY data type, short-circuting
 console.log(3 || 'Jonas');
+
 console.log('' || 'Jonas');
 console.log(true || 0);
 console.log(undefined || null);
