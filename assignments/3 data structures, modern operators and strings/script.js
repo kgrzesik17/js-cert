877fa3da4068cf16d47ae8d7903080b60005bf48
Kacper <kgrzesik17@gmail.com>
2026-01-04 16:22:39 +0100

    assignment rest pattern and parameters

--- assignments/3 data structures, modern operators and strings/script.js
+++ assignments/3 data structures, modern operators and strings/script.js
@@ -298,3 +298,18 @@ function spellWord(word) {
 
 spellWord("JavaScript");
 */
+
+// 4.1
+const [mainKeyword, ...rest] = books[0].keywords;
+console.log(mainKeyword, rest);
+
+// 4.2
+const { publisher, ...restOfTheBook } = books[1];
+console.log(publisher, restOfTheBook);
+
+// 4.3
+function printBookAuthorsCount(title, ...authors) {
+  console.log(`The book ${title} has ${authors.length} authors.`);
+}
+
+printBookAuthorsCount("Algorithms", "Robert Sedgewick", "Kevin Wayne");
