0b14fef1bb08ba1f501e3ab0d381babca4ac5f33
Kacper <kgrzesik17@gmail.com>
2026-01-05 18:41:59 +0100

    assignment looping objects

--- assignments/3 data structures, modern operators and strings/script.js
+++ assignments/3 data structures, modern operators and strings/script.js
@@ -417,3 +417,26 @@ const newBook2 = {
 console.log(getFirstKeyword(books[0]));
 console.log(getFirstKeyword(newBook2));
 */
+
+// 11.1
+const entries = [];
+
+for (const key of Object.keys(books[0].thirdParty.goodreads)) {
+  entries.push([key]);
+}
+
+console.log(entries);
+
+// 11.2
+for (const [index, value] of Object.values(
+  books[0].thirdParty.goodreads
+).entries()) {
+  entries[index].push(value);
+}
+
+// 11.3
+const entries2 = Object.entries(books[0].thirdParty.goodreads);
+
+// 11.4
+console.log(entries);
+console.log(entries2);
