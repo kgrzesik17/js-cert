ac6e37bd8724110794de35db65566a7f040ca14d
Kacper <kgrzesik17@gmail.com>
2026-01-05 18:01:10 +0100

    assignment enhanced object literals

--- assignments/3 data structures, modern operators and strings/script.js
+++ assignments/3 data structures, modern operators and strings/script.js
@@ -345,7 +345,6 @@ for (let i = 0; i < books.length; i++) {
   books[i].highlighted &&= !(books[i].thirdParty.goodreads.rating < 4.2);
   console.log(books[i].highlighted);
 }
-*/
 
 // 8.1
 let pageSum = 0;
@@ -373,3 +372,31 @@ console.log(allAuthors);
 for (const [i, el] of allAuthors.entries()) {
   console.log(`${i + 1}. ${el}`);
 }
+*/
+
+// 9.1
+const bookData = [
+  ["title", "Computer Networking: A Top-Down Approach"],
+  ["author", ["James F. Kurose", "Keith W. Ross"]],
+  ["publisher", "Addison Wesley"],
+];
+
+// Do the rest
+const newBook = {
+  [bookData[0][0]]: bookData[0][1],
+  [bookData[1][0]]: bookData[1][1],
+  [bookData[2][0]]: bookData[2][1],
+};
+
+console.log(newBook);
+
+// 9.2
+const pages = 880;
+
+const newBook2 = {
+  title: "The C Programming Language",
+  author: ["Brian W. Kernighan", "Dennis M. Ritchie"],
+  pages,
+};
+
+console.log(newBook2);
