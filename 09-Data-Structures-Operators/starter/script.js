f42818716b76491c172348591d5dddfa9414b9bb
Kacper <kgrzesik17@gmail.com>
2026-01-07 14:41:29 +0100

    maps fundamentals

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -82,6 +82,43 @@ const mexicanFoods = new Set([
   'garlic',
 ]);
 
+// MAPS
+
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+console.log(rest.set(2, 'Lisbon, Portugal'));
+
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open :D')
+  .set(false, 'We are closed :(');
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+const time = 21;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+rest.delete(2);
+
+const arr = [1, 2];
+
+rest.set(arr, 'Test');
+rest.set(document.querySelector('h1'), 'Heading');
+
+console.log(rest);
+console.log(rest.size);
+// rest.clear();
+// console.log(rest);
+
+console.log(rest.get(arr));
+
+/*
 const commonFoods = italianFoods.intersection(mexicanFoods);
 console.log('Intersection: ', commonFoods);
 console.log([...commonFoods]);
@@ -104,7 +141,6 @@ console.log('Symmetric: ', uniqueItalianAndMexicanFoods);
 
 console.log(italianFoods.isDisjointFrom(mexicanFoods));
 
-/*
 const ordersSet = new Set([
   'Pasta',
   'Pizza',
