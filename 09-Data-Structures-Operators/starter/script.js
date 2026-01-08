e3baa6394c3a8bc4f4b01c41ff743d6f23027ea1
Kacper <kgrzesik17@gmail.com>
2026-01-08 17:06:46 +0100

    stringh methods practice

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,8 +1,6 @@
 'use strict';
 
 // Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
 
@@ -82,6 +80,24 @@ const mexicanFoods = new Set([
   'garlic',
 ]);
 
+// string metrods exercise
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+const getCode = str => str.slice(0, 3).toUpperCase();
+
+for (const flight of flights.split('+')) {
+  const [type, from, to, time] = flight.split(';');
+  const output = `${type.startsWith('_Delayed') ? '🔴' : ''}${type.replaceAll(
+    '_',
+    ' '
+  )} from ${getCode(from)} to ${getCode(to)} (${time.replace(
+    ':',
+    'h'
+  )})`.padStart(36, ' ');
+  console.log(output);
+}
+
 ///////////////////////////////////////
 // Coding Challenge #4
 
@@ -114,11 +130,13 @@ Afterwards, test with your own test data!
 GOOD LUCK 😀
 */
 
+// CHALLENGE 4
+/*
 document.body.append(document.createElement('textarea'));
 document.body.append(document.createElement('button'));
 const button = document.querySelector('button');
 const text = document.querySelector('textarea').value;
-
+  
 // button.addEventListener('click', function () {
 //   const text = document.querySelector('textarea').value;
 //   const lines = [...text.split('\n')];
@@ -160,6 +178,8 @@ button.addEventListener('click', function () {
     console.log(output);
   }
 });
+
+*/
 
 // jonas' way
 // button.addEventListener('click', function () {
