01541c4fe65965753d35fda952df811a67b151d5
Kacper <kgrzesik17@gmail.com>
2026-01-09 07:41:03 +0100

    functions accepting callback functions + functions returning functiong

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -30,7 +30,7 @@ createBooking('LH123', 2);
 createBooking('LH123', 5);
 
 createBooking('LH123', undefined, 1000);
-*/
+
 
 // HOW PASSING ARGUMENTS WORKS
 
@@ -65,3 +65,67 @@ const newPassport = function (person) {
 
 newPassport(jonas);
 checkIn(flight, jonas);
+
+function oneWord(str) {
+  return str.replace(' ', '').toLowerCase();
+}
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// higher order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('JavaScript is the best', upperFirstWord);
+transformer('JavaScript is the best', oneWord);
+
+// js uses callbacks all the time
+const high5 = function () {
+  console.log('hello');
+};
+
+document.body.addEventListener('click', high5);
+
+['Jonas', 'Martha', 'Adam'].forEach(high5);
+
+// my example
+function carDealer(car, fn) {
+  return `Your new, shiny car: ${fn(car)}`;
+}
+
+function bmw(model) {
+  if (model === 'e90') {
+    return 'BMW E90';
+  } else if (model === 'e60') {
+    return 'BMW E60';
+  } else {
+    return 'other BMW';
+  }
+}
+
+console.log(carDealer('e90', bmw));
+*/
+
+function greet(greeting) {
+  return function (name) {
+    console.log(`${greeting}, ${name}!`);
+  };
+}
+
+const greeterHey = greet('Hey');
+greeterHey('Jonas');
+greeterHey('Steven');
+
+greet('Hello')('Steven');
+
+// challenge
+const greet1 = greeting => name => console.log(`${greeting}, ${name}!`);
+
+greet1('Hi')('Kacper');
