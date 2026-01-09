470ffb5f887ef21e3d5bb998e0b24ef20924af64
Kacper <kgrzesik17@gmail.com>
2026-01-09 08:24:17 +0100

    bind, call, apply

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -111,7 +111,6 @@ function bmw(model) {
 }
 
 console.log(carDealer('e90', bmw));
-*/
 
 function greet(greeting) {
   return function (name) {
@@ -129,3 +128,97 @@ greet('Hello')('Steven');
 const greet1 = greeting => name => console.log(`${greeting}, ${name}!`);
 
 greet1('Hi')('Kacper');
+*/
+
+const lufthansa = {
+  airline: 'Lufthansa',
+  iataCode: 'LH',
+  bookings: [],
+  // book: function() {}
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+    );
+    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+  },
+};
+
+lufthansa.book(239, 'Kacper Grzesik');
+lufthansa.book(635, 'John Smith');
+
+const eurowings = {
+  airline: 'Eurowings',
+  iataCode: 'EW',
+  bookings: [],
+};
+
+const book = lufthansa.book;
+
+// does not work
+// book(23, 'Sarah Williams');
+
+book.call(eurowings, 23, 'Sarah Williams');
+console.log(eurowings);
+
+book.call(lufthansa, 239, 'Mary Cooper');
+
+console.log(lufthansa);
+
+const lot = {
+  airline: 'Polskie Linie Lotnicze LOT',
+  iataCode: 'LO',
+  bookings: [],
+};
+
+book.call(lot, 123, 'Jeffrey Jenkins');
+
+// apply method
+const flightData = [583, 'George Cooper'];
+book.apply(lot, flightData);
+book.call(lot, ...flightData);
+
+// bind method
+// book.call(eurowings, 23, 'Sarah Willaims');
+
+const bookEW = book.bind(eurowings);
+const bookLH = book.bind(lufthansa);
+const bookLO = book.bind(lot);
+
+bookEW(23, 'Steven Williams');
+
+const bookEW23 = book.bind(eurowings, 23);
+bookEW23('Kacper Grzesik');
+bookEW23('Martha Cooper');
+
+// with event listeners
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+  console.log(this);
+
+  this.planes++;
+  console.log(this.planes);
+};
+
+document
+  .querySelector('.buy')
+  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+// partial application
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+const addVAT = addTax.bind(null, 0.23);
+console.log(addVAT(100));
+console.log(addVAT(23));
+
+const addTaxRate = function (rate) {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+
+console.log(addTaxRate(0.23)(100));
+
+const addVAT1 = addTaxRate(0.23);
+
+console.log(addVAT(100));
