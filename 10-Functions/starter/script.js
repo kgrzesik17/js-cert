0e9f1ad593aaabd62e32bf5d7e5db7e57df9e483
Kacper <kgrzesik17@gmail.com>
2026-01-13 15:03:56 +0100

    more closure examples

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -327,7 +327,6 @@ runOnce();
 
 // console.log(isPrivate);
 console.log(notPrivate);
-*/
 
 const secureBooking = function () {
   let passengerCount = 0;
@@ -345,3 +344,44 @@ booker();
 booker();
 
 console.dir(booker);
+*/
+
+// example 1
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+console.dir(f);
+
+// re-assigning f function
+h();
+f();
+console.dir(f);
+
+// example 2
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+boardPassengers(180, 3);
