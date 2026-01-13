27cacba0c20eea2efde447b79f6478ac47492c90
Kacper <kgrzesik17@gmail.com>
2026-01-13 17:51:38 +0100

    the filter method

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -90,10 +90,7 @@ function createUsernames(accounts) {
       .join('');
   });
 }
-
 createUsernames(accounts);
-
-console.log(accounts);
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
@@ -105,7 +102,7 @@ console.log(accounts);
 //   ['GBP', 'Pound sterling'],
 // ]);
 
-// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
@@ -267,3 +264,19 @@ const movementsDescriptions = movements.map(
 
 console.log(movementsDescriptions);
 */
+
+const deposits = movements.filter(function (mov) {
+  return mov > 0;
+});
+
+console.log(movements);
+console.log(deposits);
+
+const depositsFor = [];
+for (const mov of movements) if (mov > 0) depositsFor.push(mov);
+
+console.log(depositsFor);
+
+const withdrawals = movements.filter(mov => mov < 0);
+
+console.log(withdrawals);
