238068ec33fdc426002b86029e18bcfa698c21da
Kacper <kgrzesik17@gmail.com>
2026-02-02 17:59:20 +0100

    creating dates

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -319,7 +319,6 @@ labelBalance.addEventListener('click', function () {
     if (i % 3 === 0) row.style.backgroundColor = 'blue';
   });
 });
-*/
 
 // numeric separators
 
@@ -363,3 +362,36 @@ console.log(huge + ' is REALLY big!!!');
 // divisions
 console.log(10n / 3n);
 console.log(10 / 3);
+
+// create a date
+const now = new Date();
+console.log(now);
+
+console.log(new Date('Feb 02 2026 17:51:06'));
+console.log(new Date('December 24, 2015'));
+console.log(new Date(account1.movementsDates[0]));
+
+console.log(new Date(2037, 10, 19, 15, 23, 5));
+console.log(new Date(2037, 10, 33));
+
+console.log(new Date(0));
+console.log(new Date(3 * 24 * 60 * 60 * 1000));
+*/
+
+// working with dates
+const future = new Date(2037, 10, 19, 15, 23);
+console.log(future);
+console.log(future.getFullYear());
+console.log(future.getMonth());
+console.log(future.getDate());
+console.log(future.getHours());
+console.log(future.getMinutes());
+console.log(future.getSeconds());
+console.log(future.toISOString());
+console.log(future.getTime());
+
+console.log(new Date(2142253380000));
+console.log(Date.now());
+
+future.setFullYear(2040);
+console.log(future);
