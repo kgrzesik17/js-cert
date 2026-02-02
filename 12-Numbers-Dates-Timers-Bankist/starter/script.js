446435f8c0e6604d6282824bec663530f66e29d8
Kacper <kgrzesik17@gmail.com>
2026-02-02 17:33:40 +0100

    the reminder operator

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -269,7 +269,7 @@ console.log(Number.isNaN(+'20X'));
 console.log(Number.isNaN(23 / 0));
 
 console.log(Number.isFinite(20 / 0));
-*/
+
 
 console.log(Math.sqrt(25));
 console.log(25 ** (1 / 2));
@@ -302,3 +302,21 @@ console.log(Math.floor(23.3));
 // rounding decimals
 console.log((2.7).toFixed(3));
 console.log(+(2.345).toFixed(2));
+*/
+
+console.log(5 % 2);
+console.log(8 % 3);
+console.log(6 % 2);
+
+const isEven = n => n % 2 === 0;
+
+console.log(isEven(8));
+console.log(isEven(23));
+console.log(isEven(514));
+
+labelBalance.addEventListener('click', function () {
+  [...document.querySelectorAll('.movements__row')].forEach(function (row, i) {
+    if (i % 2 === 0) row.style.backgroundColor = 'orangered';
+    if (i % 3 === 0) row.style.backgroundColor = 'blue';
+  });
+});
