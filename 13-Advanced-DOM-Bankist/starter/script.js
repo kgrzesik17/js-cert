425165138c4c178dbe3dab3d471fee5807fd2eea
Kacper <kgrzesik17@gmail.com>
2026-02-08 18:29:50 +0100

    selecting, creating and deleting elements

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -29,3 +29,43 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+//
+// selecting elements
+
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+const header = document.querySelector('.header');
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
+
+document.getElementById('#section--1');
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+
+const allButtonsClass = document.getElementsByClassName('btn');
+const allButtonsQuery = document.querySelectorAll('btn');
+
+console.log(allButtonsClass);
+console.log(allButtonsQuery);
+
+// creating and inserting elements
+const message = document.createElement('div'); // creates and stores DOM element
+message.classList.add('cookie-message');
+// message.textContent = 'We use cookies for improved functionality and analytics.';
+message.innerHTML =
+  'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+
+// header.prepend(message); // add as a first child
+header.append(message); // add as a first child
+// header.append(message.cloneNode(true));
+
+header.before(message);
+header.after(message);
+
+// delete elements
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', () => message.remove());
