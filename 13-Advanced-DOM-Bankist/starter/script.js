6b8ddc1be6fb5a8d1eac2b0f56cdf881e1887750
Kacper <kgrzesik17@gmail.com>
2026-02-12 20:08:08 +0100

    lazy loading images

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -170,6 +170,34 @@ allSections.forEach(function (section) {
   section.classList.add('section--hidden');
 });
 
+// lazy loading images
+const imgTargets = document.querySelectorAll('img[data-src');
+
+const loadImg = function (entries, observer) {
+  entries.forEach(entry => {
+    console.log(entry);
+
+    if (!entry.isIntersecting) return;
+
+    // replace src with data-src
+    entry.target.src = entry.target.dataset.src;
+
+    // only remove the blurry filter AFTER the image loads
+    entry.target.addEventListener('load', () => {
+      entry.target.classList.remove('lazy-img');
+    });
+
+    observer.unobserve(entry.target);
+  });
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '200px',
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));
 /*
 //
 // selecting elements
