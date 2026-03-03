536eb64d913e84035dfbfbf51d29347d9cb22e79
Kacper <kgrzesik17@gmail.com>
2026-03-03 20:32:24 +0100

    prototypal inheritance on built in objects

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 // constructor functions and the new operator
 const Person = function (firstName, birthYear) {
   // instance properties
@@ -7,9 +8,9 @@ const Person = function (firstName, birthYear) {
   this.birthYear = birthYear;
 
   // never create methods inside a constructor
-  this.calcAge = function () {
-    console.log(2037 - this.birthYear);
-  };
+  // this.calcAge = function () {
+  //   console.log(2037 - this.birthYear);
+  // };
 };
 
 const jonas = new Person('Jonas', 1991);
@@ -29,3 +30,51 @@ const jay = 'Jay';
 console.log(jonas instanceof Person);
 console.log([] instanceof Array);
 console.log(jay instanceof Person);
+
+// prototypes
+console.log(Person.prototype);
+
+// the way of doing methods
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+jonas.calcAge();
+matilda.calcAge();
+
+console.log(jonas.__proto__);
+console.log(jonas.__proto__ === Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(jonas));
+console.log(Person.prototype.isPrototypeOf(matilda));
+console.log(Person.prototype.isPrototypeOf(Person));
+
+Person.prototype.species = 'Homo Sapiens';
+console.log(jonas.species, matilda.species);
+
+console.log(jonas.hasOwnProperty('firstName'));
+console.log(jonas.hasOwnProperty('species'));
+
+console.log(jonas.__proto__);
+
+// Object.prototype (top of the prototype chain)
+console.log(jonas.__proto__.__proto__);
+console.log(jonas.__proto__.__proto__.__proto__);
+
+console.dir(Person.prototype.constructor);
+
+const arr = [3, 6, 6, 6, 2, 3, 4, 5, 6, 3]; // new Array === []
+console.log(arr.__proto__);
+console.log(arr.__proto__ === Array.prototype); // true
+
+console.log(arr.__proto__.__proto__);
+
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+
+console.log(arr.unique());
+
+const h1 = document.querySelector('h1');
+console.dir(x => x + 1);
+*/
