e76b7058f762a209083aebcabac7e69e2cab5c72
Kacper <kgrzesik17@gmail.com>
2026-03-04 19:42:28 +0100

    challenge 3

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -266,7 +266,6 @@ console.log(mercedes.speedUS);
 // 2.3
 bmw.speedUS = 90;
 bmw.display();
-*/
 
 const Person = function (firstName, birthYear) {
   this.firstName = firstName;
@@ -303,3 +302,46 @@ console.log(mike instanceof Object);
 
 Student.prototype.contructor = Student;
 console.dir(Student.prototype.constructor);
+*/
+
+// 3.1
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(this.speed);
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(this.speed);
+};
+
+const EV = function (make, speed, charge) {
+  Car.call(this, make, speed);
+  this.charge = charge;
+};
+
+EV.prototype = Object.create(Car.prototype);
+
+EV.prototype.chargeBattery = function (chargeTo) {
+  this.charge = chargeTo;
+};
+
+EV.prototype.accelerate = function () {
+  this.speed += 20;
+  this.charge -= 1;
+  console.log(
+    `${this.make} going at ${this.speed} km/h, with a charge of ${this.charge}`,
+  );
+};
+
+const tesla = new EV('Tesla', 100, 90);
+console.log(tesla);
+
+tesla.chargeBattery(100);
+tesla.accelerate();
+tesla.brake();
