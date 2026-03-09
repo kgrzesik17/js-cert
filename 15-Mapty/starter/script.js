7ae94e2fad00a29acd063b999d57dd2b36d45a0e
Kacper <kgrzesik17@gmail.com>
2026-03-09 20:27:53 +0100

    displaying a map using leaflet library

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -16,6 +16,20 @@ if (navigator.geolocation) {
     function (position) {
       const { latitude, longitude } = position.coords;
       console.log(`https://www.google.pl/maps/@${latitude},${longitude}`);
+
+      const coords = [latitude, longitude];
+
+      const map = L.map('map').setView(coords, 17);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+        .openPopup();
     },
     function () {
       alert('Could not get your position');
