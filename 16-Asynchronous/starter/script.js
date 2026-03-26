5fef4e2da2098c21f27add4c21c40bf73c83a647
Kacper <kgrzesik17@gmail.com>
2026-03-26 19:02:59 +0100

    challenge 2

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -219,7 +219,6 @@ wait(1)
 
 Promise.resolve('abc').then(x => console.log(x));
 Promise.reject(new Error('abc')).catch(x => console.error(x));
-*/
 
 // promisify a callback based API
 function getPosition() {
@@ -271,3 +270,49 @@ function whereAmI() {
 }
 
 btn.addEventListener('click', whereAmI);
+*/
+
+const imgContainer = document.querySelector('.images');
+
+function wait(seconds) {
+  return new Promise(resolve => {
+    setTimeout(resolve, seconds * 1000);
+  });
+}
+
+function createImage(imgPath) {
+  return new Promise((resolve, reject) => {
+    const img = document.createElement('img');
+    img.src = imgPath;
+
+    img.addEventListener('load', () => {
+      imgContainer.append(img);
+      resolve(img);
+    });
+
+    img.addEventListener('error', () => {
+      reject(new Error('Image not found'));
+    });
+  });
+}
+
+let currentImage;
+
+createImage('img/img-1.jpg')
+  .then(img => {
+    currentImage = img;
+    console.log('Image 1 loaded');
+    return wait(2);
+  })
+  .then(() => {
+    currentImage.style.display = 'none';
+    return createImage('img/img-2.jpg');
+  })
+  .then(img => {
+    console.log(img);
+    currentImage = img;
+    console.log('Image 2 loaded');
+    return wait(2);
+  })
+  .then(() => (currentImage.style.display = 'none'))
+  .catch(err => console.error(err));
