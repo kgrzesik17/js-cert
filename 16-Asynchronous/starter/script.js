d7271f6493eb4ca366e40bebb880e5e64df43b4e
Kacper <kgrzesik17@gmail.com>
2026-03-30 20:07:49 +0100

    other promise combinators: race, allSettled, any

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -388,6 +388,7 @@ function getJSON(url, errorMsg = 'Something went wrong') {
   });
 }
 
+/*
 async function get3Countries(c1, c2, c3) {
   try {
     // const [data1] = await getJSON(`https://restcountries.com/v2/name/${c1}`);
@@ -406,3 +407,44 @@ async function get3Countries(c1, c2, c3) {
 }
 
 get3Countries('poland', 'usa', 'germany');
+*/
+
+// Promise.race - recieves [] of promises, returns a promise. Settled as soon as one of the inputs settles. First settled promise wins the race
+(async function () {
+  const res = await Promise.race([
+    getJSON(`https://restcountries.com/v2/name/italy`),
+    getJSON(`https://restcountries.com/v2/name/poland`),
+    getJSON(`https://restcountries.com/v2/name/norway`),
+  ]);
+  // console.log(res[0]);
+})();
+
+const timeout = function (sec) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error('Request took too long'));
+    }, sec * 1000);
+  });
+};
+
+Promise.race([
+  getJSON(`https://restcountries.com/v2/name/poland`),
+  getJSON(`https://restcountries.com/v2/name/norway`),
+  timeout(1),
+])
+  .then(res => console.log(res[0]))
+  .catch(err => console.error(err));
+
+// Promise.allSettled
+
+Promise.allSettled([
+  Promise.resolve('Success'),
+  Promise.reject('Error'),
+  Promise.resolve('OK!'),
+]).then(res => console.log(res));
+
+Promise.any([
+  Promise.resolve('Success'),
+  Promise.reject('Error'),
+  Promise.resolve('OK!'),
+]).then(res => console.log(res));
