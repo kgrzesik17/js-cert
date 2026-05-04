26fe3791815158c988b727ade06310f73cfa506c
Kacper <kgrzesik17@gmail.com>
2026-05-04 19:15:38 +0200

    exporting and importing in es6 modules

---
+++ 17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
@@ -0,0 +1,20 @@
+// exporting module
+console.log('Exporting module');
+
+const shippingCost = 10;
+export const cart = [];
+
+export function addToCart(product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`${quantity} ${product} added to cart`);
+}
+
+const totalPrice = 237;
+const totalQuantity = 23;
+
+export { totalPrice, totalQuantity as tq };
+
+export default function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`${quantity} ${product} added to cart`);
+}
