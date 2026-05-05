89e5ba2db6620721b26c57212685cb9a49a3e3b7
Kacper <kgrzesik17@gmail.com>
2026-05-05 17:29:30 +0200

    the module pattern

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -1,4 +1,7 @@
 'use strict';
+
+// import shoppingCart from './shoppingCart';
+
 // importing module
 
 /*
@@ -6,7 +9,6 @@ import { addToCart, totalPrice as price, tq } from './shoppingCart.js';
 addToCart('bread', 5);
 console.log(price, tq);
 console.log('Importing module');
-*/
 
 import * as ShoppingCart from './shoppingCart.js';
 ShoppingCart.addToCart('bread', 5); // like an API
@@ -40,3 +42,34 @@ console.log(lastPost);
 
 const lastPost2 = await getLastPost();
 console.log(lastPost2);
+*/
+
+const ShoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+
+  function addToCart(product, quantity) {
+    cart.push({ product, quantity });
+    console.log(
+      `${quantity} ${product} added to cart (shipping cost if ${shippingCost})`,
+    );
+  }
+
+  function orderStock(product, quantity) {
+    console.log(`${quantity} ${product} ordered from supplier`);
+  }
+
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+
+ShoppingCart2.addToCart('apple', 4);
+ShoppingCart2.addToCart('pizza', 2);
+console.log(ShoppingCart2);
+console.log(ShoppingCart2.shippingCost);
